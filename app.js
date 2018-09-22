0551ca77e06b60d4bcebd8289aea459f47d2e735
Hoa Hoang <6288799+tienhoah@users.noreply.github.com>
2018-09-21 18:02:48 -0700

    create users api for testing and some dependencies.

--- app.js
+++ app.js
@@ -1,26 +1,35 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+"use strict";
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+require('dotenv').config();
 
-var app = express();
+const createError = require('http-errors');
+const express     = require('express');
+const path        = require('path');
+
+const PORT         = process.env.PORT || 8080;
+const ENV          = process.env.ENV || "development";
+const knexConfig   = require("./knexfile");
+const knex         = require("knex")(knexConfig[ENV]);
+
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/api/users');
+
+const app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/users', usersRouter(knex));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/api/users.js
@@ -0,0 +1,17 @@
+"use stricts";
+
+const express = require('express');
+const router = express.Router();
+
+module.exports = (knex) => {
+  router.get("/", (req, res) => {
+    knex
+      .select("*")
+      .from("users")
+      .then((results) => {
+        res.json(results);
+      });
+  });
+
+  return router;
+};
