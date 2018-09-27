17a3dcef5c45e8a46331a0faac06763ee7dfbc02
Hoa Hoang <6288799+tienhoah@users.noreply.github.com>
2018-09-27 13:11:05 -0700

    make an api post to create new user.

--- routes/api/users.js
+++ routes/api/users.js
@@ -13,6 +13,24 @@ module.exports = (knex) => {
       });
   });
 
+  router.post("/", (req, res) => {
+    knex('users')
+    .returning("*")
+      .insert({
+        facebook_id: req.body.facebook_id,
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        phone: req.body.phone,
+        sex: req.body.sex,
+        age: req.body.age,
+        city: req.body.city,
+      }).then((results) => {
+        res.json(results);
+      });
+  });
+
+
   router.get("/fbid/:id", (req, res) => {
     knex
       .select("*")
