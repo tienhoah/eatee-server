a636dc4ff3cbc52501db721aad94cd02d93f99f7
veilance <christopher.tam@alumni.ubc.ca>
2018-09-27 18:24:01 -0700

    database is able to update the coupon_details table when a user clicks redeem

--- db/seeds/3_coupon_details.js
+++ db/seeds/3_coupon_details.js
@@ -7,20 +7,20 @@ exports.seed = function(knex, Promise) {
       return knex('coupon_details').insert([
         {is_active: true, is_redeemed: false, swipe: true, coupon_batch_id: 1, user_id: 3 },
         {is_active: true, is_redeemed: false, swipe: true, coupon_batch_id: 1, user_id: 2 },
-        {is_active: false, is_redeemed: true, swipe: true, coupon_batch_id: 2, user_id: 6 },
-        {is_active: true, is_redeemed: true, swipe: true, coupon_batch_id: 3, user_id: 6 },
+        {is_active: false, is_redeemed: false, swipe: true, coupon_batch_id: 2, user_id: 6 },
+        {is_active: true, is_redeemed: false, swipe: true, coupon_batch_id: 3, user_id: 6 },
         {is_active: true, is_redeemed: false, swipe: true, coupon_batch_id: 2, user_id: 6 },
         {is_active: false, is_redeemed: false, swipe: true, coupon_batch_id: 3, user_id: 7 },
-        {is_active: true, is_redeemed: true, swipe: true, coupon_batch_id: 4, user_id: 1 },
+        {is_active: true, is_redeemed: false, swipe: true, coupon_batch_id: 4, user_id: 1 },
         {is_active: true, is_redeemed: false, swipe: true, coupon_batch_id: 4, user_id: 4 },
-        {is_active: false, is_redeemed: true, swipe: true, coupon_batch_id: 5, user_id: 10 },
+        {is_active: false, is_redeemed: false, swipe: true, coupon_batch_id: 5, user_id: 10 },
         {is_active: false, is_redeemed: false, swipe: true, coupon_batch_id: 5, user_id: 8 },
         {is_active: true, is_redeemed: false, swipe: true, coupon_batch_id: 6, user_id: 1 },
         {is_active: false, is_redeemed: false, swipe: true, coupon_batch_id: 6, user_id: 3 },
-        {is_active: false, is_redeemed: true, swipe: true, coupon_batch_id: 3, user_id: 5 },
+        {is_active: false, is_redeemed: false, swipe: true, coupon_batch_id: 3, user_id: 5 },
         {is_active: false, is_redeemed: false, swipe: true, coupon_batch_id: 2, user_id: 10 },
         {is_active: true, is_redeemed: false, swipe: true, coupon_batch_id: 1, user_id: 8 },
-        {is_active: false, is_redeemed: true, swipe: true, coupon_batch_id: 4, user_id: 7 }
+        {is_active: false, is_redeemed: false, swipe: true, coupon_batch_id: 4, user_id: 7 }
       ]);
     });
 };
