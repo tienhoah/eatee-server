8ef6404cf782e9aab9ea4428afad4ce81cabfced
Hoa Hoang <6288799+tienhoah@users.noreply.github.com>
2018-09-28 16:08:45 -0700

    change foreign key user_id to user_facebook_id

--- db/migrations/20180921122037_create_users_table.js
+++ db/migrations/20180921122037_create_users_table.js
@@ -2,7 +2,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', function (table) {
     table.increments();
-    table.string('facebook_id');
+    table.string('facebook_id').unique();
     table.string('first_name');
     table.string('last_name');
     table.string('email');
