b290619262069574f8d4357cb2ee5a81beacb66a
Julia Zhou <juliazhouism@gmail.com>
2018-10-01 14:17:52 -0700

    function made to add days to expiration date

--- routes/api/restaurants.js
+++ routes/api/restaurants.js
@@ -45,64 +45,89 @@ module.exports = knex => {
   });
 
   //get swipe total for a restaurant
-  router.get("/:id/swipe", (req,res) => {
-    knex
-      .count("*")
-      .from('coupon_details')
-      .join('coupon_batches', 'coupon_details.coupon_batch_id', '=', 'coupon_batches.id')
-      .join('restaurants', 'restaurants.id', '=', 'coupon_batches.restaurant_id')
-      .where({'restaurants.id':req.params.id})
-      .andWhere({'swipe':true})
-      .then((results) => {
-        if (!results.length) {
-          res.json({error: "Not found"});
-        } else {
-          res.json(results);
-        }
-      })
-  })
-
-  //get impression total for a restaurant
-  router.get("/:id/impression", (req,res) => {
-    knex
-      .sum("impression")
-      .from("coupon_batches")
-      .join("restaurants", 'coupon_batches.restaurant_id', '=', 'restaurants.id')
-      .where({'restaurants.id':req.params.id})
-      .then((results) => {
-        if (!results.length) {
-          res.json({error: "Not found"});
-        } else {
-          res.json(results);
-        }
-      })
-  })
-
-  //get redeemed total for a restaurant
-  router.get("/:id/redeem", (req,res) => {
+  router.get("/:id/swipe", (req, res) => {
     knex
       .count("*")
       .from("coupon_details")
-      .join('coupon_batches', 'coupon_details.coupon_batch_id', '=', 'coupon_batches.id')
-      .join('restaurants', 'restaurants.id', '=', 'coupon_batches.restaurant_id')
-      .where({'restaurants.id':req.params.id})
-      .andWhere({'is_redeemed':true})
-      .then((results) => {
+      .join(
+        "coupon_batches",
+        "coupon_details.coupon_batch_id",
+        "=",
+        "coupon_batches.id"
+      )
+      .join(
+        "restaurants",
+        "restaurants.id",
+        "=",
+        "coupon_batches.restaurant_id"
+      )
+      .where({ "restaurants.id": req.params.id })
+      .andWhere({ swipe: true })
+      .then(results => {
         if (!results.length) {
-          res.json({error: "Not found"});
+          res.json({ error: "Not found" });
         } else {
           res.json(results);
         }
-      })
-  })
+      });
+  });
+
+  //get impression total for a restaurant
+  router.get("/:id/impression", (req, res) => {
+    knex
+      .sum("impression")
+      .from("coupon_batches")
+      .join(
+        "restaurants",
+        "coupon_batches.restaurant_id",
+        "=",
+        "restaurants.id"
+      )
+      .where({ "restaurants.id": req.params.id })
+      .then(results => {
+        if (!results.length) {
+          res.json({ error: "Not found" });
+        } else {
+          res.json(results);
+        }
+      });
+  });
+
+  //get redeemed total for a restaurant
+  router.get("/:id/redeem", (req, res) => {
+    knex
+      .count("*")
+      .from("coupon_details")
+      .join(
+        "coupon_batches",
+        "coupon_details.coupon_batch_id",
+        "=",
+        "coupon_batches.id"
+      )
+      .join(
+        "restaurants",
+        "restaurants.id",
+        "=",
+        "coupon_batches.restaurant_id"
+      )
+      .where({ "restaurants.id": req.params.id })
+      .andWhere({ is_redeemed: true })
+      .then(results => {
+        if (!results.length) {
+          res.json({ error: "Not found" });
+        } else {
+          res.json(results);
+        }
+      });
+  });
 
   //get restaurant id
   router.get("/yelpid/:id", (req, res) => {
     knex
       .select("*")
       .from("restaurants")
-      .where({'restaurants.yelp_id':req.params.id})
-      .then((results) => {
+      .where({ "restaurants.yelp_id": req.params.id })
+      .then(results => {
         res.json(results);
       });
   });
@@ -124,6 +149,10 @@ module.exports = knex => {
       });
   });
 
+  function addDays(theDate, days) {
+    return new Date(theDate.getTime() + days * 24 * 60 * 60 * 1000);
+  }
+
   router.post("/:id/coupon_batches", (req, res) => {
     console.log("connect to router /:id/coupon_batches");
     const {
@@ -144,7 +173,7 @@ module.exports = knex => {
         dish_name,
         description,
         timestamp,
-        time_limit,
+        time_limit: addDays(new Date(), time_limit),
         quantity,
         image,
         price,
