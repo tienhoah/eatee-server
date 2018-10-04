a38cc19b418c24906003ee038ea1213e5cc87191
Julia Zhou <juliazhouism@gmail.com>
2018-10-03 19:22:13 -0700

    seed data updated

--- db/seeds/3_coupon_details.js
+++ db/seeds/3_coupon_details.js
@@ -1,30 +1,255 @@
-
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('coupon_details').del()
-    .then(function () {
+  return knex("coupon_details")
+    .del()
+    .then(function() {
       // Inserts seed entries
-      return knex('coupon_details').insert([
-        {is_active: true, is_redeemed: false, swipe: false, coupon_batch_id: 1, user_facebook_id: null },
-        {is_active: true, is_redeemed: false, swipe: false, coupon_batch_id: 1, user_facebook_id: null },
-        {is_active: false, is_redeemed: false, swipe: false, coupon_batch_id: 2, user_facebook_id: null },
-        {is_active: true, is_redeemed: false, swipe: false, coupon_batch_id: 3, user_facebook_id: null },
-        {is_active: true, is_redeemed: false, swipe: false, coupon_batch_id: 2, user_facebook_id: null },
-        {is_active: false, is_redeemed: false, swipe: false, coupon_batch_id: 3, user_facebook_id: null },
-        {is_active: true, is_redeemed: false, swipe: false, coupon_batch_id: 4, user_facebook_id: null },
-        {is_active: true, is_redeemed: false, swipe: false, coupon_batch_id: 4, user_facebook_id: null },
-        {is_active: false, is_redeemed: false, swipe: false, coupon_batch_id: 5, user_facebook_id: null },
-        {is_active: false, is_redeemed: false, swipe: false, coupon_batch_id: 5, user_facebook_id: null },
-        {is_active: true, is_redeemed: false, swipe: false, coupon_batch_id: 6, user_facebook_id: null },
-        {is_active: false, is_redeemed: false, swipe: false, coupon_batch_id: 6, user_facebook_id: null },
-        {is_active: false, is_redeemed: false, swipe: false, coupon_batch_id: 3, user_facebook_id: null },
-        {is_active: false, is_redeemed: false, swipe: false, coupon_batch_id: 2, user_facebook_id: null },
-        {is_active: true, is_redeemed: false, swipe: false, coupon_batch_id: 1, user_facebook_id: null },
-        {is_active: false, is_redeemed: false, swipe: false, coupon_batch_id: 4, user_facebook_id: null },
-        {is_active: true, is_redeemed: false, swipe: false, coupon_batch_id: 1, user_facebook_id: null },
-        {is_active: true, is_redeemed: false, swipe: false, coupon_batch_id: 1, user_facebook_id: null },
-        {is_active: true, is_redeemed: false, swipe: false, coupon_batch_id: 2, user_facebook_id: null },
-        {is_active: true, is_redeemed: false, swipe: false, coupon_batch_id: 3, user_facebook_id: null },
+      return knex("coupon_details").insert([
+        {
+          is_active: true,
+          is_redeemed: false,
+          swipe: false,
+          coupon_batch_id: 1,
+          user_facebook_id: null
+        },
+        {
+          is_active: true,
+          is_redeemed: false,
+          swipe: false,
+          coupon_batch_id: 1,
+          user_facebook_id: null
+        },
+        {
+          is_active: true,
+          is_redeemed: false,
+          swipe: false,
+          coupon_batch_id: 1,
+          user_facebook_id: null
+        },
+        {
+          is_active: true,
+          is_redeemed: false,
+          swipe: false,
+          coupon_batch_id: 1,
+          user_facebook_id: null
+        },
+        {
+          is_active: true,
+          is_redeemed: false,
+          swipe: false,
+          coupon_batch_id: 1,
+          user_facebook_id: null
+        },
+        {
+          is_active: true,
+          is_redeemed: false,
+          swipe: false,
+          coupon_batch_id: 2,
+          user_facebook_id: null
+        },
+        {
+          is_active: true,
+          is_redeemed: false,
+          swipe: false,
+          coupon_batch_id: 2,
+          user_facebook_id: null
+        },
+        {
+          is_active: true,
+          is_redeemed: false,
+          swipe: false,
+          coupon_batch_id: 2,
+          user_facebook_id: null
+        },
+        {
+          is_active: true,
+          is_redeemed: false,
+          swipe: false,
+          coupon_batch_id: 2,
+          user_facebook_id: null
+        },
+        {
+          is_active: true,
+          is_redeemed: false,
+          swipe: false,
+          coupon_batch_id: 3,
+          user_facebook_id: null
+        },
+        {
+          is_active: true,
+          is_redeemed: false,
+          swipe: false,
+          coupon_batch_id: 3,
+          user_facebook_id: null
+        },
+        {
+          is_active: true,
+          is_redeemed: false,
+          swipe: false,
+          coupon_batch_id: 3,
+          user_facebook_id: null
+        },
+        {
+          is_active: true,
+          is_redeemed: false,
+          swipe: false,
+          coupon_batch_id: 3,
+          user_facebook_id: null
+        },
+        {
+          is_active: true,
+          is_redeemed: false,
+          swipe: false,
+          coupon_batch_id: 4,
+          user_facebook_id: null
+        },
+        {
+          is_active: true,
+          is_redeemed: false,
+          swipe: false,
+          coupon_batch_id: 4,
+          user_facebook_id: null
+        },
+        {
+          is_active: true,
+          is_redeemed: false,
+          swipe: false,
+          coupon_batch_id: 4,
+          user_facebook_id: null
+        },
+        {
+          is_active: true,
+          is_redeemed: false,
+          swipe: false,
+          coupon_batch_id: 5,
+          user_facebook_id: null
+        },
+        {
+          is_active: true,
+          is_redeemed: false,
+          swipe: false,
+          coupon_batch_id: 5,
+          user_facebook_id: null
+        },
+        {
+          is_active: true,
+          is_redeemed: false,
+          swipe: false,
+          coupon_batch_id: 5,
+          user_facebook_id: null
+        },
+        {
+          is_active: true,
+          is_redeemed: false,
+          swipe: false,
+          coupon_batch_id: 6,
+          user_facebook_id: null
+        },
+        {
+          is_active: true,
+          is_redeemed: false,
+          swipe: false,
+          coupon_batch_id: 6,
+          user_facebook_id: null
+        },
+        {
+          is_active: true,
+          is_redeemed: false,
+          swipe: false,
+          coupon_batch_id: 6,
+          user_facebook_id: null
+        },
+        {
+          is_active: true,
+          is_redeemed: false,
+          swipe: false,
+          coupon_batch_id: 6,
+          user_facebook_id: null
+        },
+        {
+          is_active: true,
+          is_redeemed: false,
+          swipe: false,
+          coupon_batch_id: 6,
+          user_facebook_id: null
+        },
+        {
+          is_active: true,
+          is_redeemed: false,
+          swipe: false,
+          coupon_batch_id: 6,
+          user_facebook_id: null
+        },
+        {
+          is_active: true,
+          is_redeemed: false,
+          swipe: false,
+          coupon_batch_id: 6,
+          user_facebook_id: null
+        },
+        {
+          is_active: true,
+          is_redeemed: false,
+          swipe: false,
+          coupon_batch_id: 7,
+          user_facebook_id: null
+        },
+        {
+          is_active: true,
+          is_redeemed: false,
+          swipe: false,
+          coupon_batch_id: 7,
+          user_facebook_id: null
+        },
+        {
+          is_active: true,
+          is_redeemed: false,
+          swipe: false,
+          coupon_batch_id: 7,
+          user_facebook_id: null
+        },
+        {
+          is_active: true,
+          is_redeemed: false,
+          swipe: false,
+          coupon_batch_id: 7,
+          user_facebook_id: null
+        },
+        {
+          is_active: true,
+          is_redeemed: false,
+          swipe: false,
+          coupon_batch_id: 8,
+          user_facebook_id: null
+        },
+        {
+          is_active: true,
+          is_redeemed: false,
+          swipe: false,
+          coupon_batch_id: 8,
+          user_facebook_id: null
+        },
+        {
+          is_active: true,
+          is_redeemed: false,
+          swipe: false,
+          coupon_batch_id: 8,
+          user_facebook_id: null
+        },
+        {
+          is_active: true,
+          is_redeemed: false,
+          swipe: false,
+          coupon_batch_id: 9,
+          user_facebook_id: null
+        },
+        {
+          is_active: true,
+          is_redeemed: false,
+          swipe: false,
+          coupon_batch_id: 9,
+          user_facebook_id: null
+        }
       ]);
     });
 };
